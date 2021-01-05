178454ccc3fb7f01b67becb0ec7d53d4375eeaea
johnnyferns14 <johnnyferns@gmail.com>
2021-01-05 08:48:05 +0000

    Foreach method added in trips.js

--- assets/trips.js
+++ assets/trips.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
             let outboundpartialdate = $("#date-dep").val();
             let currency = $("#currency").val();
         
-            let fetchId = `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/${currency}/en-US/${originplace}-sky/${destinationplace}-sky/${outboundpartialdate}?inboundpartialdate=${inboundpartialdate}`;
+            let fetchId = `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/${currency}/en-US/${originplace}-sky/${destinationplace}-sky/${outboundpartialdate}`;
 
             console.log(fetchId);
 
@@ -22,18 +22,15 @@ $(document).ready(function(){
                         }
                     });
                 const data = await res.json();
-                console.log(data);
-                const price = data.Quotes[0].MinPrice;
-                const carrierName = data.Carriers[0].Name;
-                const payCurrency = data.Currencies[0].Symbol;
-                const depPlace = data.Places[1].Name;
-                const arrPlace = data.Places[0].Name;
-                const dateDep = data.Quotes[0].OutboundLeg.DepartureDate.slice(0,10);
-                const timeDep = data.Quotes[0].OutboundLeg.DepartureDate.slice(11,16);
-                console.log(price, carrierName, depPlace, arrPlace, payCurrency, dateDep, timeDep);
-                $("#confirm").click(function(){
-                $(".search-results").append(`<div class="result"<p>Your search returned the following results: </p><p>${depPlace} - ${arrPlace} on ${dateDep} at ${timeDep}hrs will cost you ${payCurrency}${price}.<button>Click for Hotels</button></div>`);
-            });
+                const quotes = data.Quotes;
+							
+			    quotes.forEach(function(quote){
+				// quote.forEach(function(info){
+				    console.log(quote);
+				// });
+				
+			
+			});
         
             };
 
