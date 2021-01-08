824473601ccf865a1c49d9a7c88504e6ec10a897
johnnyferns14 <johnnyferns@gmail.com>
2021-01-08 04:46:50 +0000

    changes made to index.html with reference to W3C guidelines

--- assets/JS/email.js
+++ assets/JS/email.js
@@ -1,3 +1,7 @@
+(function () {
+    emailjs.init("user_MkCT1xIxJ2h1sitIi1aDu");
+})();
+
 function sendMail(contactDetails) {
 
     let templateParams = {
@@ -7,7 +11,7 @@ function sendMail(contactDetails) {
 
     emailjs.send('service_dq6by94', 'flightdetails', {
         "from_name": contactDetails.nameInput.value,
-        "user_email": contactDetails.inputEmail.value,
+        "user_email": contactDetails.email.value,
         "message": contactDetails.queryTextarea.value,
     })
         .then(function (response) {
